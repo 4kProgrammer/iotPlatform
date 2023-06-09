7d643392279e3575afb249fd500a4694158c928e
4k.programmer <4k.programmer@gmail.com>
2023-06-09 16:03:15 +0330

    add timeTable

---
+++ pages/time.tsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState, useMemo } from "react";
+import classNames from 'classnames';
+
+type TimeSlot = {
+  hour: number;
+  half: number;
+  selected: boolean;
+};
+
+const halfHours = ['00', '30'];
+
+// Replace these functions with your actual data fetching/updating methods
+const fetchTimeSlots = async (): Promise<TimeSlot[]> => {
+  // Replace with your actual data fetching method
+  // This is a placeholder that returns the initial values
+  return Array.from({ length: 24 }, (_, hour) =>
+    halfHours.map((_, i) => ({
+      hour,
+      half: i,
+      selected: false,
+    }))
+  ).flat();
+};
+
+const updateTimeSlots = async (timeSlots: TimeSlot[]): Promise<void> => {
+  // Replace with your actual data updating method
+  // This is a placeholder that does nothing
+};
+
+const TimeLimitSection: React.FC = () => {
+  const [timeSlots, setTimeSlots] = useState<TimeSlot[]>([]);
+  const [startRangeIndex, setStartRangeIndex] = useState<number | null>(null);
+  const [endRangeIndex, setEndRangeIndex] = useState<number | null>(null);
+  const [isSelecting, setIsSelecting] = useState(false);
+  const [selectingValue, setSelectingValue] = useState<boolean | null>(null);
+
+  // Fetch the time slots when the component mounts
+  useEffect(() => {
+    fetchTimeSlots().then(setTimeSlots);
+  }, []);
+
+  // Log and update the time slots when they change
+  useEffect(() => {
+    console.log(timeSlots);
+    updateTimeSlots(timeSlots);
+  }, [timeSlots]);
+
+  const startSelecting = (index: number) => {
+    setIsSelecting(true);
+    setStartRangeIndex(index);
+    setSelectingValue(timeSlots[index].selected ? false : true);
+    const newTimeSlots = [...timeSlots];
+    newTimeSlots[index].selected = selectingValue !== null ? selectingValue : !newTimeSlots[index].selected;
+    setTimeSlots(newTimeSlots);
+  };
+  const endSelecting = () => {
+    setIsSelecting(false);
+    setStartRangeIndex(null);
+    setSelectingValue(null);
+    setEndRangeIndex(null);
+  };
+
+  const selectTimeSlot = (index: number) => {
+    if (isSelecting && selectingValue !== null) {
+      const newTimeSlots = [...timeSlots];
+      const oldEndRangeIndex = endRangeIndex;
+      setEndRangeIndex(index);
+      const rangeStart = Math.min(startRangeIndex!, index);
+      const rangeEnd = Math.max(startRangeIndex!, index);
+      if (oldEndRangeIndex !== null) {
+        const oldRangeStart = Math.min(startRangeIndex!, oldEndRangeIndex);
+        const oldRangeEnd = Math.max(startRangeIndex!, oldEndRangeIndex);
+        for (let i = oldRangeStart; i <= oldRangeEnd; i++) {
+          newTimeSlots[i].selected = newTimeSlots[i].selected !== selectingValue;
+        }
+      }
+      for (let i = rangeStart; i <= rangeEnd; i++) {
+        newTimeSlots[i].selected = selectingValue;
+      }
+      setTimeSlots(newTimeSlots);
+    }
+  };
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (isSelecting) {
+        endSelecting();
+      }
+    };
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => window.removeEventListener('mouseup', handleMouseUp);
+  }, [isSelecting]);
+
+  const ranges = useMemo(() => {
+    let start: number | null = null;
+    let end: number | null = null;
+    const ranges: { start: number; end: number }[] = [];
+
+    timeSlots.forEach((slot, index) => {
+      if (slot.selected) {
+        if (start === null) {
+          start = index;
+        }
+        end = index;
+      } else if (start !== null) {
+        ranges.push({ start, end: end! });
+        start = null;
+        end = null;
+      }
+    });
+
+    return ranges;
+  }, [timeSlots]);
+
+  return (
+    <div className="mt-4 select-none">
+        <h2 className="text-lg font-semibold mb-2">TimeTable</h2>
+        <div className="relative bg-gray-100 p-4 m-0 p-0">
+            {timeSlots.map((slot, index) => {
+              const range = ranges.find(({ start, end }) => index >= start && index <= end);
+              let displayText = "";
+              if (range && range.start === index) {
+                const endSlot = range.end < timeSlots.length - 1 ? timeSlots[range.end + 1] : { hour: 24, half: 0 };
+                displayText = `${timeSlots[range.start].hour}:${halfHours[timeSlots[range.start].half]}-${endSlot.hour}:${halfHours[endSlot.half]}`;
+              }
+
+              return (
+                <div className="flex border-t border-gray-300 m-0 p-0" key={index}>
+                    <div className="border-r border-gray-300 p-2 text-center flex items-start m-0 p-0">
+                        <div className="inline-block min-w-[4rem] m-0 p-0">
+                            <span className="text-sm font-mono">{slot.hour}:{halfHours[slot.half]}</span>
+                        </div>
+                    </div>
+                    <div
+                        onMouseDown={() => startSelecting(index)}
+                        onMouseEnter={() => selectTimeSlot(index)}
+                        className={classNames('border-r border-gray-300 flex-grow h-10 cursor-pointer relative rounded transition-colors duration-300 m-0 p-0', {
+                            'bg-blue-400': slot.selected,
+                            'hover:border-blue-400': !slot.selected
+                        })}
+                    >{displayText && <span className="absolute top-1 left-0 transform translate-y-1/4 translate-x-1/2 text-black">{displayText}</span>}
+                    </div>
+                </div>
+              );
+            })}
+        </div>
+    </div>
+  );
+};
+
+export default TimeLimitSection;
